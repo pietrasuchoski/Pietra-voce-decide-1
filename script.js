a87506f8edb6d3dad429d26792ff59185bb91d88
pietrasuchoski <167487724+pietrasuchoski@users.noreply.github.com>
2024-05-29 22:49:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+  {
+    enuncido: "Pergunta 1",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enuncido: "Pergunta 2",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enuncido: "Pergunta 3",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enuncido: "Pergunta 4",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enuncido: "Pergunta 5",
+    alternativas: [
+      "Resposta 1",
+      "Resposta 2"
+    ]
+  }
+];
